cf4aa27522ddb63d0b53da34d7fdf7ee42cdbb1c
Mostafathereal <mohsem1@mcmaster.ca>
2019-11-02 14:15:53 -0400

    FaceNet model

---
+++ FaceRecModel.py
@@ -0,0 +1,14 @@
+
+from keras import backend as k
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications import InceptionResNetV2
+from keras.models import Model, model_from_json
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Flatten, Dropout, Conv2D
+from keras import optimizers
+
+from keras import layers
+from keras import models
+
+
+print("Hello")
